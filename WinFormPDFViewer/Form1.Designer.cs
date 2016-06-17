24ad0335712b17857662e3d1c11f93e8cf8c457d
Heejune Kim <heejune@gmail.com>
2016-06-17 18:31:05 +0900

    Initial commit

---
+++ WinFormPDFViewer/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WinFormPDFViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.locateBtn = new System.Windows.Forms.Button();
+            this.locateText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(561, 381);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // locateBtn
+            // 
+            this.locateBtn.Location = new System.Drawing.Point(474, 388);
+            this.locateBtn.Name = "locateBtn";
+            this.locateBtn.Size = new System.Drawing.Size(75, 23);
+            this.locateBtn.TabIndex = 1;
+            this.locateBtn.Text = "Locate";
+            this.locateBtn.UseVisualStyleBackColor = true;
+            this.locateBtn.Click += new System.EventHandler(this.locateBtn_Click);
+            // 
+            // locateText
+            // 
+            this.locateText.Location = new System.Drawing.Point(62, 390);
+            this.locateText.Name = "locateText";
+            this.locateText.Size = new System.Drawing.Size(385, 20);
+            this.locateText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 397);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PDF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 414);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.locateText);
+            this.Controls.Add(this.locateBtn);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button locateBtn;
+        private System.Windows.Forms.TextBox locateText;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinFormPDFViewer/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormPDFViewer
+{
+    // without this, ArgumentException will occur when setting ObjectForScripting to this
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+
+            locateText.TextChanged += LocateText_TextChanged;
+        }
+
+        private void LocateText_TextChanged(object sender, EventArgs e)
+        {
+            if (locateText.Text.EndsWith("pdf", true, null) == true && 
+                File.Exists(locateText.Text) == true)
+            {
+                var buffer = File.ReadAllBytes(locateText.Text);
+
+                // Document.InvokeScript seems not understanding the Blob type
+                //object[] args = { fileBytes };
+                //webBrowser1.Document.InvokeScript("PDFViewerApplication.open", args);
+
+                // so convert it to base64 and pass it
+                var asBase64 = Convert.ToBase64String(buffer);
+                webBrowser1.Document.InvokeScript("openPdfAsBase64", new[] { asBase64 });
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            // https://github.com/TaoK/PoorMansTSqlFormatter/blob/d6b4f7bedc02ce1bf59acb16dd1f49609c216aa7/PoorMansTSqlFormatterDemo/FrameworkClassReplacements/CustomContentWebBrowser.cs
+
+            //webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser1.ObjectForScripting = this;
+            webBrowser1.WebBrowserShortcutsEnabled = true;
+            var view_page = String.Format("file:///{0}/web/viewer.html", dir);
+            this.webBrowser1.Url = new Uri(view_page);
+        }
+
+        private void locateBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                locateText.Text = dlg.FileName;
+            }
+        }
+    }
+}
